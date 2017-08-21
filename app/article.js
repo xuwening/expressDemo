af3e434788ccdf238fc69eaa1e8de68f5f305bbc
xuwening <xuwening@126.com>
2017-08-21 23:58:59 +0800

    添加参数解析

--- app/article.js
+++ app/article.js
@@ -3,6 +3,13 @@ import express from 'express'
 
 let router = express.Router()
 
-router.get('/', (req, res) => res.send('article page.'))
+router.get('/:user', (req, res) => {
+
+    console.log('path:', req.params)
+    console.log('query: ', req.query)
+    console.log('body:', req.body)
+    console.log('router:', req.route)
+    res.send('article page.')
+})
 
 module.exports = router
