a3d836224fe660365e58104420aaf871ca08b320
hongjingjun <hongjingjun@leadeon.cn>
2017-08-23 10:48:10 +0800

    md5以及zip解压

--- public/uploadfile/uploadAction.js
+++ public/uploadfile/uploadAction.js
@@ -1,7 +1,23 @@
 
+import fs from 'fs'
 import path from 'path'
 import express from 'express'
 import multer from 'multer'
+import fileMd5 from './fileMd5'
+import bundlesList from './bundlesInfo'
+
+var AdmZip = require('adm-zip'); 
+
+import unzip from 'unzip'
+
+let bundleInfo = { 
+    "zipPath": "", 
+    "version": "", 
+    "moduleName": "",
+    "zipHash": "",
+    "jsbundleHash": ""
+}
+
 
 let router = express.Router()
 
@@ -9,6 +25,7 @@ let storage = multer.diskStorage({
     destination: (req, file, cb) => {
         console.log('1--------------')
         cb(null, path.join(__dirname, '../files'))
+        bundlesList.push(path.join(__dirname, '../files', file.originalname))
     },
     filename: (req, file, cb) => {
         console.log('2--------------')
@@ -19,6 +36,22 @@ let upload = multer({storage: storage})
 
 router.post('/', upload.array('avatar'), (req, res, next) => {
     console.log('3--------------')
+
+    bundlesList.map((value, index, arrs) => {
+
+        fileMd5(value, (md5str) => {
+            bundleInfo.zipHash = md5str
+            bundleInfo.moduleName = value
+        })
+
+        const unzipFolder = path.basename(value, '.zip')
+        const unzipPath = path.join(path.dirname(value), unzipFolder)
+
+        var zip = new AdmZip(value); 
+        zip.extractAllTo( unzipPath);
+
+    })
+
     res.send('文件上传成功')
 })
 
